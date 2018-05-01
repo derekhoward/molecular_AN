d908065a1cf2519c0c2ce998cd9d0eabd6f81bc5
Leon French <leonfrench@gmail.com>
2018-05-01 16:32:10 -0400

    Minor edits

--- config.py
+++ config.py
@@ -4,5 +4,8 @@ import os
 if platform.node() == 'RES-C02TQ1V6G8WL.local':
 	DATA_DIR = "/Users/derek_howard/projects/molecular_AN/data"
 	CORES = 3
+if platform.node() == 'RES-C02RF0T2.local':
+	DATA_DIR = "/Users/lfrench/Desktop/results/molecular_AN/data"
+	CORES = 3
 else :
 	raise RuntimeError('ERROR: this computer ('+platform.node()+') is not configured. Please change this in config.py')
