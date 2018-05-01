3f0534ae314a60476b0df2c17131389e61a0d6cb
Leon French <leonfrench@gmail.com>
2018-05-01 17:36:43 -0400

    Changed order so AUROC is first stat

--- HBA_analysis.py
+++ HBA_analysis.py
@@ -76,8 +76,8 @@ def generate_stats_table(exp_df, gene_list, verbose=True):
     fdr_corrected = pd.Series(fdr_corrected, index=pvalues.index)
     auc = exp_df.apply(lambda col: calc_AUC(exp_series=col, gene_list=gene_list))
 
-    table = pd.concat([pvalues, fdr_corrected, auc],
-                      keys=['p', 'pFDR', 'AUROC'], axis=1)
+    table = pd.concat([auc, pvalues, fdr_corrected],
+                      keys=['AUROC', 'p', 'pFDR', ], axis=1)
     table.set_index(exp_df.columns, inplace=True)
     return table.sort_values('AUROC', ascending=False)
 
