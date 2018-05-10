634b78b7d6d780c749a631c5334e7c909b028993
Leon French <leonfrench@gmail.com>
2018-05-10 12:35:16 -0400

    Small edits to intersection code

--- R/IntersectLists.R
+++ R/IntersectLists.R
@@ -4,13 +4,18 @@ LutterBinge <- read_csv("./data/genelists/Lutter et al. Table S4.BingeEating.hyp
 LutterRestricted <- read_csv("./data/genelists/Lutter et al. Table S3.RestrictedEating.hypenFixed.txt", col_names=F)$X1
 Duncan <- read_csv("./data/genelists/Duncan et al. rs4622308.hypenFixed.txt", col_names=F)$X1
 Negraes <- read_csv("./data/genelists/Negraes et al. Table S5.hypenFixed.txt", col_names=F)$X1
-GSE60190 <- read_csv("./data/genelists/GSE60190.reanalysis.txt", col_names=F)$X1
 
-venn( list(LutterRestricted=LutterRestricted, Duncan=Duncan, Negraes=Negraes, GSE60190 = GSE60190) )
+intersect(Negraes, LutterRestricted)
+intersect(Negraes, LutterBinge)
 
-combined <- union(union(union(LutterRestricted, Duncan),Negraes),GSE60190)
+venn( list(LutterRestricted=LutterRestricted, Duncan=Duncan, Negraes=Negraes) )
+venn( list(LutterRestricted=LutterRestricted, Duncan=Duncan, Negraes=Negraes, LutterBinge=LutterBinge) )
+
+combined <- union(union(union(LutterRestricted, Duncan),Negraes))
 
 write_csv(tbl_df(sort(unique(combined))), "./data/genelists/Four.combined.txt", col_names = F)
 
 
-
+length(Negraes)
+length(LutterBinge)
+length(LutterRestricted)
