11132041f3ba1815cd7d2158b7ab657ee026b6d1
Leon French <leonfrench@gmail.com>
2018-05-15 16:34:54 -0400

    Fixes for rank for RFNG

--- R/secondary limited use code/RFNG.R
+++ R/secondary limited use code/RFNG.R
@@ -1,9 +1,22 @@
+#short bit of code to get RFNG rankings
+library(magrittr)
+library(dplyr)
+library(readr)
+library(reshape2)
+
 geneOfInterest <- "RFNG"
 rfng <- read_tsv("./data/processed/adult_brainarea_vs_genes_exp_reannotator.tsv")
-z <- filter(rfng, gene_symbol==geneOfInterest)
-z <- melt(z) %>% as_tibble()
-z %>% arrange(-value)
+z <- melt(rfng) %>% as_tibble()
+z %<>% group_by(variable) %>% mutate(rank = rank(value))
+
+#z %>% filter(variable == "lateral parabrachial nucleus")
+
+z <- filter(z, gene_symbol==geneOfInterest)
+#z <- melt(z) %>% as_tibble()
+z %>% arrange(-rank)
 threshold <- z %>% filter(variable == 'lateral parabrachial nucleus') %>% .$value
+
+rfng <- read_tsv("./data/processed/adult_brainarea_vs_genes_exp_reannotator.tsv")
 
 rfng %>% select(gene_symbol, `lateral parabrachial nucleus`) %>% arrange(-`lateral parabrachial nucleus`) 
 
